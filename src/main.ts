c2fa3bcfa03d9f5bd5f5393bb815df99492aa412
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-03-21 23:54:33 +0800

    feat: configure swagger and api reference

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,48 @@
 import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from "@nestjs/platform-express";
-// import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-// import { apiReference } from "@scalar/nestjs-api-reference";
-// import { patchNestJsSwagger } from "nestjs-zod";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { apiReference } from "@scalar/nestjs-api-reference";
+import { patchNestJsSwagger } from "nestjs-zod";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    const app = await NestFactory.create(AppModule, {
         bufferLogs: true
     });
 
-    // registerApiReference(app);
+    patchNestJsSwagger();
+
+    const config = new DocumentBuilder()
+        .setTitle("Some Nest.js Template")
+        .setDescription("Hello, world!")
+        .setVersion("0.0.0")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    app.use(
+        "/reference",
+        apiReference({
+            content: document,
+            hideClientButton: false,
+            showSidebar: true,
+            theme: "default",
+            layout: "modern",
+            isEditable: false,
+            isLoading: false,
+            hideModels: false,
+            hideDownloadButton: false,
+            hideTestRequestButton: false,
+            hideDarkModeToggle: false,
+            hideSearch: false,
+            withDefaultFonts: true
+        })
+    );
 
     const port: number = Number(process.env.API_PORT) || 3000;
     const hostname: string = process.env.API_HOSTNAME ?? "0.0.0.0";
 
     await app.listen(port, hostname);
 }
-
-// function registerApiReference(app: NestExpressApplication): void {
-//     patchNestJsSwagger();
-
-//     const config = new DocumentBuilder().setTitle("Hello, world!").setDescription("WIP").setVersion("0.0.0").build();
-//     const document = SwaggerModule.createDocument(app, config);
-// }
 
 void bootstrap();
